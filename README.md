1f85c5e1d3a99f217730c1ae93cc145447ef7e2d
Zach Stednick <stednick@gmail.com>
2018-11-19 14:23:08 -0800

    improve R code, better README

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-This is a repository of base64 encoded HTML maps of every US
-Congressional District as well as geoJSON files for all districts. I used the [US Census
-Bureau](https://www.census.gov/geo/maps-data/data/cbf/cbf_cds.html) to
-download the Congressional Districts for the current Congress (at this
-time the 115th Congress). I then used [leaflet](https://leafletjs.com/) to make
+This is a repository of base64 encoded HTML Leaflet maps of every US
+Congressional District as well as geoJSON files for all corresponding Congressional districts. I used the [US Census
+Bureau](https://www.census.gov/geo/maps-data/data/cbf/cbf_cds.html)
+website to
+download a [shape file](http://www2.census.gov/geo/tiger/GENZ2017/shp/cb_2017_us_cd115_500k.zip) of the US Congressional Districts for the current Congress (at this
+time the 115th Congress). I then used [R](https://cran.r-project.org/) as well a wrapper for [Leaflet](https://leafletjs.com/) to make
 the maps and [geojson.io](https://github.com/mapbox/geojson.io) to make the [geoJSON](http://geojson.org/) files.
+
+For example, I live in WA-09 and here is the corresponding map:
+
+
--- make_CD_map.R
+++ make_CD_map.R
@@ -2,6 +2,11 @@ library(dplyr)
 library(sp)
 library(leaflet)
 
+census_file <- download.file("http://www2.census.gov/geo/tiger/GENZ2017/shp/cb_2017_us_cd115_500k.zip",
+                             destfile = "cb_2017_us_cd115_500k.zip")
+unzip("cb_2017_us_cd115_500k.zip")
+
+#fetch FIPS codes for all state lookups
 FIPS_code <- xml2::read_html("https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code") %>% 
   rvest::html_nodes("table") %>% 
   .[[1]] %>% 
@@ -13,14 +18,14 @@ all_US <- rgdal::readOGR("cb_2017_us_cd115_500k.shp", "cb_2017_us_cd115_500k")
 
 all_US_FIPS <- merge(all_US, FIPS_code)
 all_US_FIPS$state_district <- paste(all_US_FIPS$ALPHA, all_US_FIPS$CD115FP, sep="-")
-for(sd in all_US_FIPS$state_district){
-  temp <- subset(all_US_FIPS, state_district == sd)
+for(x in all_US_FIPS$state_district){
+  temp <- subset(all_US_FIPS, state_district == x)
   temp_map <- leaflet(temp) %>%
     addProviderTiles("OpenStreetMap.Mapnik") %>%
     addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
                 opacity = 1.0, fillOpacity = 0.5)
-  htmlwidgets::saveWidget(temp_map, file = paste0(sd, ".html"))
-  geojsonio::geojson_write(temp, file= paste0("geojson_files/", sd, ".geojson"))
+  htmlwidgets::saveWidget(temp_map, file = paste0(x, ".html"))
+  geojsonio::geojson_write(temp, file= paste0("geojson_files/", x, ".geojson"))
 }
 
 system("mv *.html html_maps")
